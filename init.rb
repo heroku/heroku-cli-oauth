a862fc5f4c66e47b6a18b389b127a66a8bc6b0d6
Pedro Belo <pedro@heroku.com>
2012-11-30 14:14:36 -0800

    fix load path

--- init.rb
+++ init.rb
@@ -1,2 +1,3 @@
-require "./lib/clients"
-require "./lib/authorizations"
+require File.expand_path("../lib/clients", __FILE__)
+require File.expand_path("../lib/authorizations", __FILE__)
+require File.expand_path("../lib/tokens", __FILE__)
