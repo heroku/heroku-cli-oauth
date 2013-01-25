cf92ce1e49c50d98e17ff0c43eb901e5d407fc7f
Brandur <brandur@mutelight.org>
2013-01-25 14:02:16 -0800

    Create OAuth authorizations instead of tokens

--- lib/authorizations.rb
+++ lib/authorizations.rb
@@ -3,14 +3,29 @@
 class Heroku::Command::Authorizations < Heroku::Command::Base
   # authorizations
   #
-  # list authorizations
+  # List authorizations
   #
   def index
     authorizations = json_decode(heroku.get("/oauth/authorizations"))
     styled_header("Authorizations")
     styled_array(authorizations.map do |auth|
-      [auth["id"], auth["client"]["name"], auth["scope"]]
+      [auth["id"], auth["description"], auth["scopes"].join(", ")]
     end)
+  end
+
+  # authorizations:create
+  #
+  # Create a new authorization giving access to your Heroku account
+  #
+  # -d, --description DESCRIPTION # set a custom authorization description
+  # -s, --scope SCOPE             # set a custom OAuth scope
+  #
+  def create
+    token = json_decode(heroku.post("/oauth/authorizations", options))
+    puts "Created OAuth authorization"
+    puts "  Description: #{token["description"]}"
+    puts "  Scope:       #{token["scopes"]}"
+    puts "  Token:       #{token["tokens"][0]["access_token"]}"
   end
 
   # authorizations:revoke [ID]
@@ -20,6 +35,6 @@ class Heroku::Command::Authorizations < Heroku::Command::Base
   def revoke
     id = shift_argument || raise(Heroku::Command::CommandFailed, "Usage: clients:destroy [ID] [options]")
     auth = json_decode(heroku.delete("/oauth/authorizations/#{id}"))
-    puts "Revoked authorization from #{auth["client"]["name"]}"
+    puts "Revoked authorization from '#{auth["description"]}'"
   end
 end
