831b195714374a7a44ff1f82db95c267a563cb7f
Brandur <brandur@mutelight.org>
2013-02-04 15:55:59 -0800

    Add `heroku sessions` for session management from the CLI

--- init.rb
+++ init.rb
@@ -1,2 +1,3 @@
 require File.expand_path("../lib/clients", __FILE__)
 require File.expand_path("../lib/authorizations", __FILE__)
+require File.expand_path("../lib/sessions", __FILE__)
---
+++ lib/sessions.rb
@@ -0,0 +1,26 @@
+# manage currently logged in OAuth sessions
+#
+class Heroku::Command::Sessions < Heroku::Command::Base
+  # sessions
+  #
+  # List active sessions for your account
+  #
+  def index
+    sessions = json_decode(heroku.get("/oauth/sessions"))
+    styled_header("OAuth Sessions")
+    styled_array(sessions.map { |session|
+      [session["id"], session["description"]]
+    })
+  end
+
+  # sessions:destroy [ID]
+  #
+  # destroy (i.e. log out) session identified by the ID
+  #
+  def destroy
+    id = shift_argument ||
+      raise(Heroku::Command::CommandFailed, "Usage: sessions:destroy [ID]")
+    session = json_decode(heroku.delete("/oauth/sessions/#{id}"))
+    puts "Destroyed #{session["id"]}"
+  end
+end
