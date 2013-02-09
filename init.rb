6d13b4897f6bc4ca28dfc1c6560b3a95f225cfc5
Pedro Belo <pedro@heroku.com>
2013-02-08 19:14:00 -0800

    add heroku tokens to list all tokens on the account

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+require "time"
 require File.expand_path("../lib/clients", __FILE__)
 require File.expand_path("../lib/authorizations", __FILE__)
 require File.expand_path("../lib/sessions", __FILE__)
+require File.expand_path("../lib/tokens", __FILE__)
