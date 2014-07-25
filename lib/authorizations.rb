31b9b232d51e680952301f31f4641af7c688cc3c
Brandur <brandur@mutelight.org>
2014-07-25 12:27:55 -0700

    Add `authorizations:update` for PATCH'ing existing authorizations

--- lib/authorizations.rb
+++ lib/authorizations.rb
@@ -41,6 +41,40 @@ class Heroku::Command::Authorizations < Heroku::Command::Base
       ).body
     end
     puts "Created OAuth authorization."
+    puts "  Client:      #{token["client"] ? token["client"]["name"] : "<none>"}"
+    puts "  ID:          #{token["id"]}"
+    puts "  Description: #{token["description"]}"
+    puts "  Scope:       #{token["scope"].join(", ")}"
+    puts "  Token:       #{token["access_token"]["token"]}"
+  end
+
+  # authorizations:update
+  #
+  # Create a new authorization giving access to your Heroku account
+  #
+  # -d, --description DESCRIPTION       # set a custom authorization description
+  #     --client-id CLIENT_ID           # identifier of OAuth client to set
+  #     --client-secret CLIENT_SECRET   # secret of OAuth client to set
+  #
+  def update
+    id = shift_argument || raise(Heroku::Command::CommandFailed, "Usage: authorizations:update [ID] [options]")
+    params = {
+      options[:client_id] ?
+        { id: options[:client_id], secret: options[:client_secret] } :
+        nil,
+      description: options[:description]
+    }
+    token = request do
+      api.request(
+        :body    => encode_json(options),
+        :expects => 200,
+        :headers => headers,
+        :method  => :patch,
+        :path    => "/oauth/authorizations/#{CGI.escape(id)}"
+      ).body
+    end
+    puts "Updated OAuth authorization."
+    puts "  Client:      #{token["client"] ? token["client"]["name"] : "<none>"}"
     puts "  ID:          #{token["id"]}"
     puts "  Description: #{token["description"]}"
     puts "  Scope:       #{token["scope"].join(", ")}"
